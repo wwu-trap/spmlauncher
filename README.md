9f2609a779a9f7b88f459c6df93aeac0f1a5a609
Kelvin <kelvins1995@gmail.com>
2017-07-14 17:53:02 +0200

    readme and doc stuff

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # README #
 
-Requirements
+##Requirements
 * Maven
 * Java 8
 
-Setup
-/tmp/SPMLauncher must have chmod 777
+##Build
+compile with:
+mvn package 
+
+##Setup
+create /tmp/SPMLauncher with chmod 777
+create /opt/applications/SPMLauncher/ManagedSoftware/spm with spm installations
+create /opt/applications/SPMLauncher/ManagedSoftware/toolbox with toolboxes
+Example:
+.
+├── spm
+│   ├── spm12
+│   ├── spm5
+│   └── spm8
+└── toolbox
+    ├── spm12
+    │   ├── cat
+    │   │   ├── v1
+    │   │   ├── v2
+    │   │   ├── v3
+    │   │   └── v4
+    │   └── TFCE
+    │       ├── v2.3
+    │       ├── v2.5
+    │       └── v2.7
+    ├── spm5
+    │   └── TFCE
+    │       └── v2.3
+    └── spm8
+        ├── cat
+        │   └── v1
+        └── TFCE
+            ├── v2.3
+            └── v2.5
+Every toolbox needs a subdirectory in the toolbox-directory of the spm installation with the excact same name!
+ 
--- src/main/java/de/wwu/trap/SpmLauncher/OSHandler.java
+++ src/main/java/de/wwu/trap/SpmLauncher/OSHandler.java
@@ -4,8 +4,20 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.UUID;
 
+/**
+ * This class holds static methods to do stuff related to the OS like
+ * manipulating the filesystem
+ * 
+ * @author Kelvin Sarink
+ */
 public class OSHandler {
 
+	/**
+	 * Returns all the spm installations. toString method of files overwritten
+	 * to only return name of file and not absolute path
+	 * 
+	 * @return Paths to the spm installtions
+	 */
 	public static File[] getSpmVersions() {
 		File spmDir = new File(App.MANAGED_SOFTWARE_DIR, "spm");
 		File[] spms = spmDir.listFiles();
@@ -26,95 +38,118 @@ public class OSHandler {
 		return spms;
 	}
 
-	public void createMounts(File spmDir, File[] toolboxes) {
+	/**
+	 * This method mounts the spm installation wth the chosen toolboxes to a
+	 * temporary directory (e.g.
+	 * App.MOUNT_DIR/6d9e5da2-cd28-43c8-af46-9f5e3e29d7de), so spm can be
+	 * started with and only with the specified toolboxses
+	 * 
+	 * @param spmDir
+	 *            the path to the chosen spm installation
+	 * @param toolboxes
+	 *            the paths to the chosen versions of the toolboxes
+	 */
+	public boolean createMounts(File spmDir, File[] toolboxes) {
+		//TODO complete createMounts
+		boolean ret = false;
 		UUID uuid = UUID.randomUUID();
 
 		File uuidDir = new File(App.MOUNT_DIR, "/" + uuid.toString());
 		uuidDir.mkdirs();
 
+		return ret;
 	}
 
-	public static void main(String[] args) {
-		UUID uuid = UUID.randomUUID();
-		File oldDir = new File("/opt/applications/SPMLauncher/ManagedSoftware/toolbox");
-		File newDir = new File(App.MOUNT_DIR + "/" + uuid.toString());
-//		System.out.println(newDir + " " + newDir.mkdirs());
-
-		mount(oldDir, newDir);
-		
-		try {
-			Thread.sleep(8000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		umount(newDir);
-	}
-
-	public static void umount(File dir) {
+	/**
+	 * Tries to unmount dir. dir has to be subdir of App.MOUNT_DIR Tries to
+	 * unmount with sudo App.MOUNT_SCRIPT -u
+	 * 
+	 * @param dir
+	 *            the dir which will be unmounted
+	 * @return whether the unmount was successfull
+	 */
+	public static boolean umount(File dir) {
+		boolean ret = false;
 		String path = dir.getAbsolutePath();
 		if (!path.startsWith(App.MOUNT_DIR + "/")) {
-			return;
+			return false;
 		}
 		String relativePath = path.replaceFirst(App.MOUNT_DIR + "/", "");
 
-		String[] cmd = new String[] { "sudo", App.MOUNT_SCRIPT, "-u", relativePath};
+		String[] cmd = new String[] { "sudo", App.MOUNT_SCRIPT, "-u", relativePath };
 
 		try {
-			/*Process p =*/ new ProcessBuilder(cmd).start();
+			Process p = new ProcessBuilder(cmd).start();
 
-//			BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-//			String line = "";
-//			while ((line = br.readLine()) != null) {
-//				System.out.println(line);
-//			}
+			// BufferedReader br = new BufferedReader(new
+			// InputStreamReader(p.getErrorStream()));
+			// String line = "";
+			// while ((line = br.readLine()) != null) {
+			// System.out.println(line);
+			// }
 
-//			System.out.println(p.waitFor());
+			ret = p.waitFor() == 0;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
 		}
-		
+
 		boolean empty = dir.listFiles().length == 0;
-		if(dir.exists() && dir.isDirectory() && empty){
+		if (dir.exists() && dir.isDirectory() && empty) {
 			dir.delete();
 		}
-		
+		return ret;
 	}
 
-	public static void mount(File oldDir, File newDir) {
+	/**
+	 * Tries to mount (via rebind) oldDir to newDir. oldDir has to be subdir of
+	 * App.MANAGED_SOFTWARE_DIR abd newDir has to be subdir of App.MOUNT_DIR
+	 * Tries to mount with sudo App.MOUNT_SCRIPT -m
+	 * 
+	 * @param oldDir
+	 *            source of the mount
+	 * @param newDir
+	 *            target of the mount
+	 * @return Whether the mount was successfull
+	 */
+	public static boolean mount(File oldDir, File newDir) {
+		boolean ret = false;
 		String oldPath = oldDir.getAbsolutePath();
 		if (!oldPath.startsWith(App.MANAGED_SOFTWARE_DIR + "/")) {
-			return;
+			return false;
 		}
 		String oldRelativePath = oldPath.replaceFirst(App.MANAGED_SOFTWARE_DIR + "/", "");
 
 		String newPath = newDir.getAbsolutePath();
 		if (!newPath.startsWith(App.MOUNT_DIR + "/")) {
-			return;
+			return false;
 		}
 		String newRelativePath = newPath.replaceFirst(App.MOUNT_DIR + "/", "");
 
 		if (!newDir.mkdirs() && !newDir.exists()) {
 			System.err.println("Could not create" + newDir.getAbsolutePath());
-			return;
+			return false;
 		}
 
 		String[] cmd = new String[] { "sudo", App.MOUNT_SCRIPT, "-m", oldRelativePath, newRelativePath };
 
 		try {
-			/*Process p =*/ new ProcessBuilder(cmd).start();
+			Process p = new ProcessBuilder(cmd).start();
 
-//			BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-//			String line = "";
-//			while ((line = br.readLine()) != null) {
-//				System.out.println(line);
-//			}
-//
-//			System.out.println(p.waitFor());
+			// BufferedReader br = new BufferedReader(new
+			// InputStreamReader(p.getErrorStream()));
+			// String line = "";
+			// while ((line = br.readLine()) != null) {
+			// System.out.println(line);
+			// }
+			//
+			ret = p.waitFor() == 0;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
 		}
 
+		return ret;
 	}
 
 }
