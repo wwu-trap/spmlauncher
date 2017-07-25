87dc818a8ba126d8abf66e4d36cc51ee9167d26e
Kelvin <kelvins1995@gmail.com>
2017-07-25 21:44:51 +0200

    only list dirs in getSpmVersions() and toString overwrite to FileManip..

--- src/main/java/de/wwu/trap/SpmLauncher/OSHandler.java
+++ src/main/java/de/wwu/trap/SpmLauncher/OSHandler.java
@@ -22,27 +22,12 @@ public class OSHandler {
 	 */
 	public static File[] getSpmVersions() {
 		File spmDir = new File(App.MANAGED_SOFTWARE_DIR, "spm");
-		File[] spms = spmDir.listFiles();
-		// Hiermit wird die toString Methode der File Objekte, sodass in der
-		// JComboBox nicht der ganze Pfad angegeben wird.
-		for (int i = 0; i < spms.length; i++) {
-			spms[i] = new File(spms[i].toString()) {
-				private static final long serialVersionUID = 2344589028794682568L;
-
-				@Override
-				public String toString() {
-					return this.getName();
-				}
-
-			};
-		}
+		File[] spms = spmDir.listFiles((e) -> e.isDirectory());
+		FileManipulator.onlyNameInToString(spms);
 		Arrays.sort(spms, new FileComparator<File>());
 		return spms;
 	}
 
-	/**
-	 * 
-	 */
 	public static boolean startSPM() {
 		boolean ret = false;
 
